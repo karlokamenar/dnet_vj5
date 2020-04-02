85e02ccbe21fe7737d8e97c388b6ebeb5a667f0a
Karlo Kamenar <kakamena@vsite.hr>
2020-04-02 14:43:42 +0200

    Add project files.

---
+++ dnet_vj5/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace dnet_vj5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dodajZabuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajZabuToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(136, 26);
+            // 
+            // dodajZabuToolStripMenuItem
+            // 
+            this.dodajZabuToolStripMenuItem.Name = "dodajZabuToolStripMenuItem";
+            this.dodajZabuToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.dodajZabuToolStripMenuItem.Text = "Dodaj Zabu";
+            this.dodajZabuToolStripMenuItem.Click += new System.EventHandler(this.dodajZabuToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bara";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dodajZabuToolStripMenuItem;
+    }
+}
+
---
+++ dnet_vj5/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dnet_vj5
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Zaba> sveZabe = new Dictionary<string, Zaba>();
+        Zaba zaba;
+        Label label1;
+        public int broj = 1;
+        int cnt = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void dodajZabuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            zaba = new Zaba();
+            cnt++;
+            zaba.Naziv = "Žaba" + broj++.ToString();
+            sveZabe.Add(zaba.Naziv, zaba);
+            label1 = new Label
+            {
+                Location = Cursor.Position,
+                Text = zaba.Naziv,
+                AutoSize = true
+            };
+            this.Controls.Add(label1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Click += new EventHandler((s, b) =>
+            {
+                if (cnt == 1)
+                    MessageBox.Show("hoću još žaba", "Roda");
+                this.Controls.Remove(label1);
+                cnt--;
+                sveZabe.Remove(label1.Text);
+
+
+            });
+
+        }
+    }
+}
